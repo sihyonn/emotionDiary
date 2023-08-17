b2829c84c263310a22c4cd95c224d5f73186b02f
sihyonn <sihyeon982@naver.com>
2023-08-17 14:52:32 +0900

    feat: 일기 data 상태관리 추가

--- src/App.js
+++ src/App.js
@@ -1,37 +1,79 @@
+import { useReducer, useRef } from "react";
+
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
-import Button from "./components/Button";
-import Header from "./components/Header";
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [...action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
 
 const App = () => {
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
+
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1; //데이터 충돌 방지
+  };
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({
+      type: "REMOVE",
+      targetId,
+    });
+  };
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
-        <Header
-          headText={"App"}
-          left={<Button text={"왼쪽버튼"} onClick={() => alert("왼쪽눌림")} />}
-          right={
-            <Button text={"오른쪽버튼"} onClick={() => alert("오른쪽눌림")} />
-          }
-        />
-
-        <h1 className="text-3xl font-bold">App.js</h1>
-        <Button
-          text={"버튼"}
-          onClick={() => alert("버튼클릭")}
-          type={"positive"}
-        />
-        <Button
-          text={"버튼"}
-          onClick={() => alert("버튼클릭")}
-          type={"negative"}
-        />
-        <Button text={"버튼"} onClick={() => alert("버튼클릭")} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -13,7 +13,7 @@ const Edit = () => {
     <div>
       <h2>이곳은 일기 수정 페이지~</h2>
       <button onClick={() => setSearchParams({ who: "winter" })}>QS</button>
-      <button onClick={() => navigate("/home")}>Home으로 바로가기</button>
+      <button onClick={() => navigate("/")}>Home으로 바로가기</button>
       <button onClick={() => navigate(-1)}>뒤로가기</button>
     </div>
   );
