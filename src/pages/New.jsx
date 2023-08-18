18faccc23769fecbfae1295d9d421dc5f3ec62bd
sihyonn <sihyeon982@naver.com>
2023-08-18 23:05:47 +0900

    feat: New Page(2) 날짜설정 구현

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 
+const getStringDate = (date) => {
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  return `${year}-${month}-${day}`;
+};
+
 const New = () => {
   const navigate = useNavigate();
+  const [date, setDate] = useState(getStringDate(new Date()));
 
   return (
     <div>
@@ -19,6 +34,18 @@ const New = () => {
           />
         }
       />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div>
+            <input
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
