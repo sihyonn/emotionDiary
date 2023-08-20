2d5b1d9990af459ccdbd863b23be2916ee36ae29
sihyonn <sihyeon982@naver.com>
2023-08-20 18:48:58 +0900

    feat: Diary Page(2)오늘의 감정(스타일링 x) 구현

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -5,7 +5,8 @@ import { DiaryStateContext } from "../App";
 import Header from "../components/Header";
 import Button from "../components/Button";
 
-import { getStringDate } from "../util/date.js";
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotionList";
 
 const Diary = () => {
   const { id } = useParams();
@@ -34,6 +35,10 @@ const Diary = () => {
   if (!data) {
     return <div>로딩중입니다...</div>;
   } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(it.emotion_id) === parseInt(data.emotion)
+    );
+
     return (
       <div>
         <Header
@@ -55,6 +60,20 @@ const Diary = () => {
             />
           }
         />
+
+        {/* 여기서부터 콘텐츠라고 알려주는 시맨틱태그 article */}
+        <article>
+          <section>
+            <h4>오늘의 감정</h4>
+            <div>
+              <img
+                src={curEmotionData.emotion_img}
+                alt={`${curEmotionData.emotion_desc} 이미지`}
+              />
+              <div>{curEmotionData.emotion_desc}</div>
+            </div>
+          </section>
+        </article>
       </div>
     );
   }
